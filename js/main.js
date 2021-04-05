bf66376698d3b6bcfc4d44d6f26d0fe17b93d713
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-04-05 16:29:29 +0200

    Functionality for search bar implemented

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const filterBtns = Array.from(document.querySelectorAll(".filter-btn-row .filter-btn"))
-const shoppingItems = Array.from(document.querySelectorAll(".shopping-item"));
+const shoppingItems = Array.from(document.querySelectorAll(".shopping-item"))
+const searchBtn = document.querySelector(".search-bar .icon-box")
+const searchBar = document.getElementById("search-input")
 
 // adds listeners for filter buttons
 filterBtns.forEach(function(el) {
@@ -7,9 +9,17 @@ filterBtns.forEach(function(el) {
         // passes regexp because search bar using regexp
         let itemNameRegExp = new RegExp(el.dataset.itemName);
         filterShoppingItems(itemNameRegExp);
-
     })
 })
+
+// adds listener for searchBtn
+searchBtn.addEventListener('click', function(event) {
+    filterShoppingItems(searchBarGetRegex())
+})
+
+function searchBarGetRegex() {
+    return new RegExp(searchBar.value,'i')
+}
 
 function filterShoppingItems(regex) {
     shoppingItems.forEach(function(el) {
