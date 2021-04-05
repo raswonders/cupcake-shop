5dd29099a6a5bc7b369116d625fdd21bb8db243f
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-04-05 14:23:50 +0200

    JS filter-btns in progress

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const filterBtns = Array.from(document.querySelectorAll(".filter-btn-row .filter-btn"))
+const shoppingItems = Array.from(document.querySelectorAll(".shopping-item"));
+
+// adds listeners for filter buttons
+filterBtns.forEach(function(el) {
+    el.addEventListener('click', function(event) {
+        filterShoppingItems(el.dataset.itemName);
+    })
+})
+
+function filterShoppingItems(itemName) {
+    shoppingItems.forEach(function(el) {
+        if (itemName == "all" || itemName == el.dataset.itemName) {
+            el.hidden = false
+        } else {
+            el.hidden = true
+        }
+    })
+}
