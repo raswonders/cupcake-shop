2e92291a48a45203e45ba7f29c5ce97eb882e693
Rastislav Hepner <rastislav.hepner@gmail.com>
2021-05-25 16:55:41 +0200

    updateTotal() implemented

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,7 @@ const cartBtn = document.querySelector('.basket-preview')
 const cart = document.querySelector('.cart-outer-box')
 const clearBtn = document.querySelector('.cart-btns').children[0]
 const checkoutBtn = document.querySelector('.cart-btns').children[1]
-const cartItemsList = document.querySelector('.cart-items')
+const cartItemsList = Array.from(document.querySelector('.cart-items').children)
 
 
 // clearBtn.addEventListener('click', function clearCart(e) {
@@ -12,3 +12,12 @@ const cartItemsList = document.querySelector('.cart-items')
 cartBtn.addEventListener('click', function cartSwitch(e) {
         cart.classList.toggle('hide')
 })
+
+function updateTotal() {
+        var sum = 0;
+     // count items and total in cart
+        cartItemsList.forEach(function addToSum(el) {
+               sum += Number(el.querySelector('.price'))
+        })
+     // update total in cart
+}
